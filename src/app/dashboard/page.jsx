652c048e881f9c9435fa18356b0fa59d9d39233c
alirzaghlmpr <alirzaghlmpr@gmail.com>
2023-09-24 23:30:44 +0330

    add api call for dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import DesktopDasboard from "@/components/Dashboard/DesktopDasboard";
 import MobileDashboard from "@/components/Dashboard/MobileDashboard";
 import QRcodeGenerator from "@/components/QRcodeGenerator";
@@ -8,6 +8,23 @@ import { useWindowSize } from "@uidotdev/usehooks";
 import { UserNavbarItems } from "@/constants";
 const Dashboard = () => {
   const { width } = useWindowSize();
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await fetch(
+        "https://qrcode-auth-system.onrender.com/api/v1/admin/createQRcode/64f10b25fec5e426ebdd4d73"
+      );
+
+      return response.ok ? response.json() : new Error("failed to fetch data");
+    };
+
+    try {
+      let r = fetchData();
+      console.log(r);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   return width < 768 ? (
     <MobileDashboard navItems={UserNavbarItems}>
       <QRcodeGenerator />
