0b56e831a477e8f530f14de17237f1b52ad41f80
alirzaghlmpr <alirzaghlmpr@gmail.com>
2023-10-13 17:47:47 +0330

    add pwa stuff

--- src/apis/CreateQRcode.js
+++ src/apis/CreateQRcode.js
@@ -11,3 +11,33 @@ const createQRcode = async (personID) => {
 };
 
 export default createQRcode;
+
+
+/**
+ * 
+ * 
+ * 
+ * const insertUser = async (data) => {
+  console.log(data);
+  try {
+    let response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/admin/person/create`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(data),
+      }
+    );
+
+    return response;
+  } catch (err) {
+    return err;
+  }
+};
+
+export default insertUser;
+
+
+ */
