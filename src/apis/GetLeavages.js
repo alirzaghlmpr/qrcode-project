9116077966a74b59c1ac015f947f9399681522e6
alirzaghlmpr <alirzaghlmpr@gmail.com>
2023-10-15 21:37:43 +0330

    fix admin leavage form

--- src/apis/GetLeavages.js
+++ src/apis/GetLeavages.js
@@ -1,7 +1,7 @@
 const getLeavages = async () => {
   try {
     let response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/admin/people`
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/admin/vacations`
     );
     return response;
   } catch (err) {
--- src/components/templates/LeavageFormAdmin/LeavageFormAdmin.jsx
+++ src/components/templates/LeavageFormAdmin/LeavageFormAdmin.jsx
@@ -1,127 +1,160 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { DateObject } from "react-multi-date-picker";
+import persian from "react-date-object/calendars/persian";
+import persian_fa from "react-date-object/locales/persian_fa";
 import CheckIcon from "@mui/icons-material/Check";
 import CloseIcon from "@mui/icons-material/Close";
 import Button from "@/components/shared/Button";
-
+import getLeavages from "@/apis/GetLeavages";
+import PageStatus from "@/constants/PageStatus";
+import LeavageStatus from "@/models/LeavageStatus";
+import LeavageType from "@/models/LeavageType";
+import updateLeavage from "@/apis/PutLeavage";
 const LeavageFormAdmin = () => {
+  const [pageStatus, setPageStatus] = useState(PageStatus.Init);
+  const [leavages, setLeavages] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setPageStatus(PageStatus.Loading);
+
+      let response = await getLeavages();
+      let result = await response.json();
+      console.log(result);
+      setLeavages(result.vacations.reverse());
+      setPageStatus(PageStatus.Fetched);
+    };
+
+    try {
+      fetchData();
+    } catch (err) {
+      setPageStatus(PageStatus.Error);
+      console.log(err);
+    }
+  }, []);
+
+  const updateLeavages = async () => {
+    try {
+      setPageStatus(PageStatus.Loading);
+      let response = await getLeavages();
+      let result = await response.json();
+      setLeavages(result.vacations.reverse());
+      setPageStatus(PageStatus.Fetched);
+    } catch (err) {
+      console.log(err);
+      setPageStatus(PageStatus.Error);
+    }
+  };
+
+  const updateUserLeavage = async (id, person, status) => {
+    try {
+      setPageStatus(PageStatus.Loading);
+
+      let response = await updateLeavage(person._id, {
+        id: id,
+        status: status,
+      });
+      let result = await response.json();
+      setPageStatus(PageStatus.Fetched);
+
+      await updateLeavages();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="grid grid-cols-1 w-full gap-4">
-      <div className="bg-slate-50 flex flex-col gap-6 p-4 md:h-[auto] h-[350px] overflow-y-scroll costume-scroll">
+      <div className="bg-slate-50 flex flex-col gap-6 p-4 md:h-[auto] h-[350px]">
         <p>درخواست های مرخصی </p>
         <hr />
-        <ol className="list-decimal px-8">
-          <li className="text-sm my-8">
-            <span>نوع مرخصی : </span>
-            <span>ساعتی</span>
-            <span> ، </span>
-            <span>تاریخ : </span>
-            <span>1402/10/12</span>
-            <span> ، </span>
-            <span>مدت : </span>
-            <span>2 ساعت</span>
-            <span> ، </span>
-            <span>نام : </span>
-            <span>علیرضا غلامپور</span>
-            <span> ، </span>
-            <span>کد پرسنلی : </span>
-            <span>3981231076</span>
-            <span> ، </span>
-            <span>وضعیت : </span>
-            <span className="text-lime-500 font-bold">تایید شده</span>
-          </li>
-          <li className="text-sm my-8">
-            <span>نوع مرخصی : </span>
-            <span>ساعتی</span>
-            <span> ، </span>
-            <span>تاریخ : </span>
-            <span>1402/10/12</span>
-            <span> ، </span>
-            <span>مدت : </span>
-            <span>2 ساعت</span>
-            <span> ، </span>
-            <span>نام : </span>
-            <span>علیرضا غلامپور</span>
-            <span> ، </span>
-            <span>کد پرسنلی : </span>
-            <span>3981231076</span>
-            <span> ، </span>
-            <span>وضعیت : </span>
-            <span className="text-amber-500 font-bold">در حال بررسی</span>
-            <p className="flex gap-4 mt-2">
-              <Button className="flex items-center gap-2 text-sm text-slate-50 bg-green-700 rounded-lg px-3 py-1">
-                <span>
-                  <CheckIcon />
-                </span>
-                <span>تایید</span>
-              </Button>
+        <ol className="list-decimal px-8 overflow-y-scroll costume-scroll h-[250px]">
+          {pageStatus === PageStatus.Loading ? (
+            <p> درحال بارگذاری ... </p>
+          ) : (
+            leavages.map(
+              ({ _id, type, duration, status, vacationDate, person }) => {
+                return (
+                  <li key={_id} className="text-sm my-2">
+                    <span>نوع مرخصی : </span>
+                    <span>
+                      {type === LeavageType.Daily ? "روزانه" : "ساعتی"}
+                    </span>
+                    <span> ، </span>
+                    <span>تاریخ : </span>
+                    <span>
+                      {new DateObject(vacationDate)
+                        .convert(persian, persian_fa)
+                        .format()}
+                    </span>
+                    <span> ، </span>
+                    <span>مدت : </span>
+                    <span>{`${duration} ${
+                      type === LeavageType.Daily ? "روز" : "ساعت"
+                    }`}</span>
+                    <span> ، </span>
+                    <span>درخواست کننده : </span>
+                    <span>{`${person && person.fname} ${
+                      person && person.lname
+                    }`}</span>
+                    <span> ، </span>
+                    <span>وضعیت : </span>
+                    <span
+                      className={`font-bold ${
+                        status === LeavageStatus.Accepted
+                          ? "text-lime-500"
+                          : status === LeavageStatus.Rejected
+                          ? "text-red-700"
+                          : "text-amber-500"
+                      }`}>
+                      {`${
+                        status === LeavageStatus.Accepted
+                          ? "تایید شده"
+                          : status === LeavageStatus.Rejected
+                          ? "رد شده"
+                          : "در حال بررسی"
+                      }`}
+                      {status === LeavageStatus.Pending && (
+                        <p className="flex gap-4 mt-2">
+                          <Button
+                            onClickCallback={() => {
+                              updateUserLeavage(
+                                _id,
+                                person,
+                                LeavageStatus.Accepted
+                              );
+                            }}
+                            className="flex items-center gap-2 text-sm text-slate-50 bg-green-700 rounded-lg px-3 py-1">
+                            <span>
+                              <CheckIcon />
+                            </span>
+                            <span>تایید</span>
+                          </Button>
 
-              <Button className="flex items-center gap-2 text-sm text-slate-50 bg-red-700 rounded-lg px-3 py-1">
-                <span>
-                  <CloseIcon />
-                </span>
-                <span>رد</span>
-              </Button>
-            </p>
-          </li>
-          <li className="text-sm my-8">
-            <span>نوع مرخصی : </span>
-            <span>ساعتی</span>
-            <span> ، </span>
-            <span>تاریخ : </span>
-            <span>1402/10/12</span>
-            <span> ، </span>
-            <span>مدت : </span>
-            <span>2 ساعت</span>
-            <span> ، </span>
-            <span>نام : </span>
-            <span>علیرضا غلامپور</span>
-            <span> ، </span>
-            <span>کد پرسنلی : </span>
-            <span>3981231076</span>
-            <span> ، </span>
-            <span>وضعیت : </span>
-            <span className="text-lime-500 font-bold">تایید شده</span>
-          </li>
-          <li className="text-sm my-8">
-            <span>نوع مرخصی : </span>
-            <span>ساعتی</span>
-            <span> ، </span>
-            <span>تاریخ : </span>
-            <span>1402/10/12</span>
-            <span> ، </span>
-            <span>مدت : </span>
-            <span>2 ساعت</span>
-            <span> ، </span>
-            <span>نام : </span>
-            <span>علیرضا غلامپور</span>
-            <span> ، </span>
-            <span>کد پرسنلی : </span>
-            <span>3981231076</span>
-            <span> ، </span>
-            <span>وضعیت : </span>
-            <span className="text-red-700 font-bold">رد شده</span>
-          </li>
-          <li className="text-sm my-8">
-            <span>نوع مرخصی : </span>
-            <span>ساعتی</span>
-            <span> ، </span>
-            <span>تاریخ : </span>
-            <span>1402/10/12</span>
-            <span> ، </span>
-            <span>مدت : </span>
-            <span>2 ساعت</span>
-            <span> ، </span>
-            <span>نام : </span>
-            <span>علیرضا غلامپور</span>
-            <span> ، </span>
-            <span>کد پرسنلی : </span>
-            <span>3981231076</span>
-            <span> ، </span>
-            <span>وضعیت : </span>
-            <span className="text-lime-500 font-bold">تایید شده</span>
-          </li>
+                          <Button
+                            onClickCallback={() =>
+                              updateUserLeavage(
+                                _id,
+                                person,
+                                LeavageStatus.Rejected
+                              )
+                            }
+                            className="flex items-center gap-2 text-sm text-slate-50 bg-red-700 rounded-lg px-3 py-1">
+                            <span>
+                              <CloseIcon />
+                            </span>
+                            <span>رد</span>
+                          </Button>
+                        </p>
+                      )}
+                    </span>
+                  </li>
+                );
+              }
+            )
+          )}
         </ol>
       </div>
     </div>
